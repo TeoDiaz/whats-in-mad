5a1e50f3a8821fbc5615d12c6f9a26d81cdfd94c
Chris Lopez <pagarizabal2@gmail.com>
2018-09-26 21:09:33 +0200

    revert to latest version

--- routes/index.js
+++ routes/index.js
@@ -8,34 +8,13 @@ const syncDB = require("../public/javascripts/syncdb")
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  axios
-    .get("https://datos.madrid.es/egob/catalogo/216619-0-wifi-municipal.json")
-    .then(info => {
-      info.data["@graph"].forEach(e => {
-        info = new Wifi({
-          title: e.title,
-          location: {
-            latitude: e.location.latitude,
-            longitude: e.location.longitude
-          },
-          organization: {
-            accesibility: e.organization.accesibility,
-            schedule: e.organization.schedule,
-            services: e.organization.services
-          }
-        });
-        info.save().then(res=>{
-          console.log("Informacion guardada de puta madre")
-        }).catch(err => {
-          console.log(err)
-        })
-      });
-      Wifi.find().then(info => {
-        res.render("index", { user: req.user, info, infoStr: JSON.stringify(info)}).catch(e => console.log(e));
-      })
-      
-    });
+  Wifi.find().then(info=>{
+  syncDB();
+  res
+    .render("index", { user: req.user, info, infoStr: JSON.stringify(info) })
+    .catch(e => console.log(e));
   })
+});
 
  
 router.get("/new", (req, res, next) => {
