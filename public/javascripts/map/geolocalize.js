f68a8bb06e515e69dd3883fb37912c6beec9059b
TeoDiaz <tdiazsan@gmail.com>
2018-09-26 20:42:29 +0200

    nothing interest

--- public/javascripts/map/geolocalize.js
+++ public/javascripts/map/geolocalize.js
@@ -1,6 +1,7 @@
 let input = document.addEventListener(
   "DOMContentLoaded",
   () => {
+    let newMarkers = [];
     var map = new google.maps.Map(document.getElementById("map"), {
       center: {
         lat: -34.397,
@@ -47,46 +48,68 @@ let input = document.addEventListener(
               radius: rad
             });
           }
-          $('#slide').on('input change', function(){
-            console.log(this.value)
-            rad = parseInt(this.value)
-            circle.setRadius(rad)
-            $(this).next($('.slider_label')).html(this.value);
-            
-            printInfoMarkers()
-          });
-          // document.getElementById("slide").onchange = function() {
-          //     circle.setRadius(rad);
-          //     console.log(parseInt(rad))
-          //   }
-          
-          
-          let printInfoMarkers = ()=>{
-          info.forEach(item => {
-            let itemPos = new google.maps.LatLng(
-              item.location.latitude,
-              item.location.longitude
-            );
-            let myPos = new google.maps.LatLng(pos);
-            if (
-              google.maps.geometry.spherical.computeDistanceBetween(
-                myPos,
-                itemPos
-              ) < circle.radius
-            ) {
-              new google.maps.Marker({
-                position: {
-                  lat: item.location.latitude,
-                  lng: item.location.longitude
-                },
-                map: map,
-                title: item.title
-              });
+          $("#slide").on("input change", function() {
+            console.log(this.value);
+            rad = parseInt(this.value);
+            circle.setRadius(rad);
+            $(this)
+              .next($(".slider_label"))
+              .html(this.value);
+            removeMarkers();
+            printInfoMarkers();
+            if (rad < 600) {
+              map.setZoom(15);
+              console.log("este es el "+rad)
+            }
+            if (rad < 1000) {
+              map.setZoom(14);
+            }
+            if (rad > 1500) {
+              map.setZoom(13);
+            }
+            if (circle.radius > 2000) {
+              map.setZoom(12);
+            }
+            if (circle.radius > 2500) {
+              map.setZoom(11);
+            }
+            if (circle.radius > 3000) {
+              map.setZoom(10);
             }
           });
-        }
-        printInfoMarkers()
 
+          let printInfoMarkers = () => {
+            info.forEach(item => {
+              let itemPos = new google.maps.LatLng(
+                item.location.latitude,
+                item.location.longitude
+              );
+              let myPos = new google.maps.LatLng(pos);
+              if (
+                google.maps.geometry.spherical.computeDistanceBetween(
+                  myPos,
+                  itemPos
+                ) < circle.radius
+              ) {
+                marker = new google.maps.Marker({
+                  position: {
+                    lat: item.location.latitude,
+                    lng: item.location.longitude
+                  },
+                  map: map,
+                  title: item.title
+                });
+                newMarkers.push(marker);
+              }
+            });
+          };
+          printInfoMarkers();
+
+          let removeMarkers = () => {
+            for (let i = 0; i < newMarkers.length; i++) {
+              newMarkers[i].setMap(null);
+            }
+          };
 
         },
         function() {
